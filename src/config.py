145bec29877bdb37af42ccc8ea5664f11f4fbcec
ashtonbcs <ashtonb.cs@gmail.com>
2026-01-18 15:18:31 -0500

    Config Revert

--- src/config.py
+++ src/config.py
@@ -5,14 +5,14 @@ BASE_DIR = Path(__file__).resolve().parents[1]
 
 DATA_DIR = BASE_DIR / "data"
 
-DATABASE_URL = "postgresql://podcast_retention_db_user:PUAe9EnFWNtjd42B9pmuDVtTlTgbtlgi@dpg-d5l96cm3jp1c739609mg-a/podcast_retention_db"
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 
-#if not DATABASE_URL:
- #   DATABASE_URL = "postgresql+psycopg2://poduser:poduser@localhost:5433/podcastdb"
+if not DATABASE_URL:
+    DATABASE_URL = "postgresql+psycopg2://poduser:poduser@localhost:5433/podcastdb"
 
-#if DATABASE_URL.startswith("postgres://"):
- #   DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 
 
