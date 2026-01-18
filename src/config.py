873e981e8a184e12089189c5867b8e95042d14d3
ashtonbcs <ashtonb.cs@gmail.com>
2026-01-18 15:03:05 -0500

    File Update

--- src/config.py
+++ src/config.py
@@ -5,7 +5,14 @@ BASE_DIR = Path(__file__).resolve().parents[1]
 
 DATA_DIR = BASE_DIR / "data"
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql+psycopg2://poduser:poduser@localhost:5433/podcastdb"
-)
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+
+if not DATABASE_URL:
+    DATABASE_URL = "postgresql+psycopg2://poduser:poduser@localhost:5433/podcastdb"
+
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+
+
