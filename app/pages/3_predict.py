9f005e9561d7f6b7ed6fa4ea10ab4d742ca810c4
ashtonbcs <ashtonb.cs@gmail.com>
2026-01-17 20:31:53 -0500

    File Update

--- app/pages/3_predict.py
+++ app/pages/3_predict.py
@@ -4,8 +4,8 @@ from src.ml.predict import predict_log
 st.title("Podcast Retention Prediction")
 
 with st.form("predict_form"):
-    length_minutes = st.number_input("Episode length (minutes)", min_value=0.0, max_value=130.0, value=62)
-    intro_length_seconds = st.number_input("Intro length (seconds)", min_value=0.0, max_value=230.0, value=113)
+    length_minutes = st.number_input("Episode length (minutes)", min_value=0.0, max_value=130.0, value=62.0)
+    intro_length_seconds = st.number_input("Intro length (seconds)", min_value=0.0, max_value=230.0, value=113.0)
     adsNumber = st.number_input("Number of ads", min_value=0, max_value=7,value=5, step=1)
     previous_ep_retention = st.number_input("Previous episode retention", min_value=0.0, max_value=80.0, value=41.0)
     host_energy = st.number_input("Host energy", min_value=0.0, max_value=10.0, value=5.0)
